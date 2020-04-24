4bc8dc652fc6674854bbebd6a7e14d900da3f668
AbdAlazez Ahmed <45468098+AbdAlazezAhmed@users.noreply.github.com>
2020-04-24 17:10:00 +0200

    Update RandomizingTime(Chapter_3).jl

--- RandomizingTime(Chapter_3).jl
+++ RandomizingTime(Chapter_3).jl
@@ -7,8 +7,8 @@ main:
 using StatsBase
 #--2-a--#
 println("\n\nQusetion 2 a\n");
-# n=sample(1:100, 5, replace = false);
-n= [64 93 70 33 55];
+n=sample(1:100, 5, replace = false);
+# n= [64 93 70 33 55];
 t_max=81;
 t_min=31;
 println(string(length(n)," random numbers between 0 and 100 are : ",n));
@@ -16,8 +16,8 @@ t=round.(Int,t_min.+n.*(t_max-t_min)./100);
 println(string("The corresponding time is : ",t));
 #--2-a--#
 println("\n\nQusetion 2 b\n");
-# n=sample(1:100, 5, replace = false);
-n=[45 93 96 1 73];
+n=sample(1:100, 5, replace = false);
+# n=[45 93 96 1 73];
 t_mean=12;
 println(string(length(n)," random numbers between 0 and 100 are : ",n));
 t=round.(Int,t_mean.*log.(100 ./(100 .-n)));
